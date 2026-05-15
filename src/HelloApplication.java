b215655244a54a05204c560b4d3c47a7771f22a5
dijaysniped <heh7no@gmail.com>
2026-05-15 17:57:10 +0500

    required changes for gui

--- src/HelloApplication.java
+++ src/HelloApplication.java
@@ -1,21 +1,16 @@
-package com.example.messagingprojoop;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.*;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+        
     @Override
     public void start(Stage stage) {
         stage.setTitle("Discorf");
@@ -144,9 +139,12 @@ public class HelloApplication extends Application {
                                 "-fx-padding: 10 15;"
                 );
                 return field;
+                
             }
 
+        public static void main(String[] args) {
+                launch(args); 
+        }
 
-
-    }
+}
 
--- src/HelloController.java
+++ src/HelloController.java
@@ -1,5 +1,3 @@
-package com.example.messagingprojoop;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
